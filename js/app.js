963012973e1ebfbb4d38742970dcdc6f0974f467
ptong19 <kindaconan@gmail.com>
2019-06-07 01:17:24 -0700

    fix math() and add li

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@
 // Go Easy write simple code , Do not overthinking to solve it.
 
 //global variables
-var cataLogArr =[];
+Product.cataLogArr =[];
 var lastDisplayed =[];
 var totalVotes = 0;
+
+
+
 
 //// render 3 images to the DOM
 var picSection = document.getElementById('picSection');
@@ -14,19 +17,20 @@ var centerProduct = document.getElementById('center');
 var rightProduct = document.getElementById('right');
 // results
 var resultsEl = document.getElementById('results');
+var resultList = document.getElementById('results2s');
 
-// Constructor and Instances
+// Constructor
 
-function Product (imageName, filePath) {
-  this.imageName = imageName;
-  this.filePath = filePath;
-  this.tallyClicked = 0;
-  this.tallyDisplayed = 0;
+function Product (imgName,filepath) {
+  this.imageName = imgName;
+  this.filePath = filepath;
+  this.votes = 0;
+  this.views = 0;
   //Push the object to Array
-  cataLogArr.push(this);
+  Product.cataLogArr.push(this);
 }
 
-
+// Instances
 new Product ('R2D2 Bag', 'img/bag.jpg');
 new Product ('Banana Slicer', 'img/banana.jpg');
 new Product ('iPad TP Stand', 'img/bathroom.jpg');
@@ -41,53 +45,32 @@ new Product ('Utensil Pen', 'img/pen.jpg');
 new Product ('Pet Sweeper', 'img/pet-sweep.jpg');
 new Product ('Pizza Scissors', 'img/scissors.jpg');
 new Product ('Shark Sleeper', 'img/shark.jpg');
-new Product ('Baby Sweeper', 'img/sweep.jpg');
+new Product ('Baby Sweeper', 'img/sweep.png');
 new Product ('Tauntaun Sleeper', 'img/tauntaun.jpg');
 new Product ('Unicorn Meat', 'img/unicorn.jpg');
-new Product ('Tentacle USB', 'img/usb.jpg');
+new Product ('Tentacle USB', 'img/usb.gif');
 new Product ('Watering Can', 'img/water-can.jpg');
 new Product ('Wine Glass', 'img/wine-glass.jpg');
-new Product ('Willenium Album', 'img/willenium.jpg');
 
 
 
 
 
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+// FUNCTION DECLARATIONS
+// ++++++++++++++++++++++++++++++++++++++++++++
 var randIndex1;
 var randIndex2;
 var randIndex3;
 
-
-
 function randGenerator() {
-  randIndex1 = Math.floor(Math.random() * cataLogArr.length-1);
-  randIndex2 = Math.floor(Math.random() * cataLogArr.length-1);
-  randIndex3 = Math.floor(Math.random() * cataLogArr.length-1);
-};
-
-
-
-
-function renderBestProduct(){
-  // create an element
-  // add content
-  // append to the parent
-
-  var bestProduct;
-  var temp = 0;
-
-  for(var i = 0; i < cataLogArr.length; i++){
-    if(cataLogArr[i].tallyClicked > temp){
-      temp = cataLogArr[i].tallyClicked;
-      bestProduct = cataLogArr[i];
-    }
-  }
-
-
-  var h2El = document.createElement('h2');
-  h2El.textContent = `The Best Product is ${bestProduct.imageName} with ${bestProduct.tallyClicked} votes.`;
-  resultsEl.appendChild(h2El);
+  randIndex1 = Math.floor(Math.random() * (Product.cataLogArr.length-1) );
+  randIndex2 = Math.floor(Math.random() * (Product.cataLogArr.length-1) );
+  randIndex3 = Math.floor(Math.random() * (Product.cataLogArr.length-1));
 }
+
+
 
 //Function to Load Images to Page
 function loadImages() {
@@ -97,64 +80,127 @@ function loadImages() {
 
   lastDisplayed = [];
 
+
   lastDisplayed.push(randIndex1);
   lastDisplayed.push(randIndex2);
   lastDisplayed.push(randIndex3);
 
-  //Re-assigning the variables for each picture
+
+  // //Re-assigning the variables for each picture
   randGenerator();
+ 
 
   //While loop to prevent double choices AND no prior choice repeats
   // this sh**t difficult things ever driving me crazyy 2hr. !!!
-  // I would better name new var called Var BigStupidNumber2 !!
+  // I would better name  called Var BigStupidcrazyNumber2 !!
+
+  // Ahren credit solution .include , splice ('')
+  // For{ var i= 0}
+  //   while
+  //   while
+
+
+
+  // switch
+
   while (randIndex1 === lastDisplayed[0] || randIndex1 === lastDisplayed[1] || randIndex1 === lastDisplayed[2] || randIndex2 === lastDisplayed[0] || randIndex2 === lastDisplayed[1] || randIndex2 === lastDisplayed[2] || randIndex3 === lastDisplayed[0] || randIndex3 === lastDisplayed[1] || randIndex3 === lastDisplayed[2] || randIndex1 === randIndex2 || randIndex1 === randIndex3 || randIndex2 === randIndex3) {
-    randGenerator();
-  }
+  randGenerator();}
+
+
+  console.log('/////////////////////////////////////////');
+  console.log('Product.cataLogArr:', Product.cataLogArr);
+  console.log('randIndex2', randIndex2);
+  console.log('randIndex1', randIndex1);
+  console.log('randIndex3', randIndex3);
+
+
   //Makes leftImg's src property equal to the fileName of the indexed item
-  leftProduct.src = cataLogArr[randIndex1].filePath;
-  centerProduct.src = cataLogArr[randIndex2].filePath;
-  rightProduct.src = cataLogArr[randIndex3].filePath;
+  leftProduct.src = Product.cataLogArr[randIndex2].filePath;
+  centerProduct.src = Product.cataLogArr[randIndex1].filePath;
+  rightProduct.src = Product.cataLogArr[randIndex3].filePath;
 
   //Adds 1 to the display tally property of the indexed object
-  cataLogArr[randIndex1].tallyDisplayed ++;
-  cataLogArr[randIndex2].tallyDisplayed ++;
-  cataLogArr[randIndex3].tallyDisplayed ++;
+  Product.cataLogArr[randIndex1].views++;
+  Product.cataLogArr[randIndex2].views++;
+  Product.cataLogArr[randIndex3].views++;
 
 }
 
 //Event Handler for click on picture
 function handleUserClick(event) {
-  event.preventDefault();
 
   if (event.target.id === 'left') {
-    cataLogArr[randIndex1].tallyClicked ++;
+    Product.cataLogArr[randIndex1].votes++;
   }
 
   else if (event.target.id === 'center') {
-    cataLogArr[randIndex2].tallyClicked ++;
+    Product.cataLogArr[randIndex2].votes++;
   }
 
   else if (event.target.id === 'right') {
-    cataLogArr[randIndex3].tallyClicked ++;
+    Product.cataLogArr[randIndex3].votes++;
   }
   else {
     alert('Pick a product!');
-    return;
+    totalVotes--;
   }
   console.log('I clicked' + event.target.id);
 
-  totalVotes += 1;
+  totalVotes++;
 
   loadImages();
+}
+
+function renderBestProduct(){
+  // create an element
+  // add content
+  // append to the parent
+
+  var bestProduct;
+  var temp = 0;
+
+  for(var i = 0; i < Product.cataLogArr.length; i++){
+    if(Product.cataLogArr[i].votes > temp){
+      temp = Product.cataLogArr[i].votes;
+      bestProduct = Product.cataLogArr[i];
+    }
+  }
+
+
+  var h2El = document.createElement('h2');
+  h2El.textContent = `The Best Product is ${bestProduct.imageName} with ${bestProduct.votes} votes.`;
+  resultsEl.appendChild(h2El);
+
+
+  var liEl = document.createElement('li');
+  liEl.textContent = `Images 1 ${Product.cataLogArr[0].imageName} : ${Product.cataLogArr[0].votes} votes.`;
+  resultList.appendChild(liEl);
+
+  liEl = document.createElement('li');
+  liEl.textContent = `Images 2 ${Product.cataLogArr[1].imageName} : ${Product.cataLogArr[1].votes} votes.`;
+  resultList.appendChild(liEl);
+
+
+  liEl = document.createElement('li');
+  liEl.textContent = `Images 3 : ${Product.cataLogArr[2].votes} votes.`;
+  resultList.appendChild(liEl);
+
+
+  liEl = document.createElement('li');
+  liEl.textContent = `Images 4 : ${Product.cataLogArr[3].votes} votes.`;
+  resultList.appendChild(liEl);
+
+  liEl = document.createElement('li');
+  liEl.textContent = `Images 5 : ${Product.cataLogArr[3].votes} votes.`;
+  resultList.appendChild(liEl);
+
+  picSection.removeEventListener('click', handleUserClick);
 }
 
 randGenerator();
 loadImages();
 
-// ++++++++++++++++++++++++++++++++++++++++++++
-// EVENT LISTENERS
-// ++++++++++++++++++++++++++++++++++++++++++++
 
+// EVENT LISTENERS
 //Listener for click-on-pic
 picSection.addEventListener('click', handleUserClick);
-
